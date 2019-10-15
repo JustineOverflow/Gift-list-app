b80bbe0898f1e5651e2d65c2608a72f23aa98f58
Justine Fabre <justinefabre84@gmail.com>
2019-10-15 19:23:28 +0100

    Bootstrap of Node JS project

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/create', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'create.html'));
+});
+
+router.post('/create', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
